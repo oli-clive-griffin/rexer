a0b3470e7c35e6989ea979d499dca20775c6e88d
oli clive-griffin <o.clivegriffin@gmail.com>
2023-10-21 21:32:40 +0800

    fix main

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,14 @@ mod interpreter;
 mod runtime_value;
 
 fn main() {
-    // let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
-    // if args.len() < 2 {
-    //     panic!("Please provide a file path to execute");
-    // }
+    if args.len() < 2 {
+        panic!("Please provide a file path to execute");
+    }
 
-    // let file_path = &args[1];
-    // let contents = std::fs::read_to_string(file_path).expect("Something went wrong reading the file").trim().to_owned();
-    let contents = "(if 0 (* 1 2) (fn (a) (* a 2)))".to_owned();
+    let file_path = &args[1];
+    let contents = std::fs::read_to_string(file_path).expect("Something went wrong reading the file").trim().to_owned();
 
     let tokens = lexer::lex(&contents);
     let ast = parser::parse(tokens);
