67c1136b81ae0b806c381f9f4980c02054945d94
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-04-17 09:29:28 +1200

    strings!

--- src/vm.rs
+++ src/vm.rs
@@ -1,6 +1,7 @@
-// #![allow(unused_variables, unused_imports, dead_code, unused_mut, unused_assignments, unused_unsafe, unused_must_use, unused_parens, unused_import_braces, private_interfaces)]
+#![allow(unused, dead_code)]
 
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::alloc::{alloc, dealloc, Layout};
 
 /// THOUGHTS
 /// First thought is that we may be able to mirror evaluator.~.eval with "produce bytecode that "
@@ -9,40 +10,61 @@ use num_enum::{IntoPrimitive, TryFromPrimitive};
 pub struct VM {
     ip: *const u8,
     // code: Vec<u8>,
-    pub stack: Vec<Value>, // todo remove pub
-                           // function_table: Vec<ByteCodeFunction>,
+    // function_table: Vec<ByteCodeFunction>,
+    pub stack: Vec<StackValue>, // todo remove pub
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Value {
+#[derive(Debug, Clone, PartialEq)]
+pub enum ObjectValue {
+    String(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct HeapObject {
+    next: *mut HeapObject,
+    value: ObjectValue,
+    // marked: bool,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum StackValue {
     Integer(i64),
     Float(f64),
     Boolean(bool),
     Nil,
+    Object(*mut HeapObject),
 }
 
-impl Value {
+#[derive(Debug, Clone, PartialEq)]
+pub enum ConstantsValue {
+    Integer(i64),
+    Float(f64),
+    Boolean(bool),
+    Nil,
+    Object(ObjectValue),
+}
+
+impl StackValue {
     fn truthy(&self) -> bool {
         match self {
-            Value::Integer(i) => *i != 0,
-            Value::Float(f) => *f != 0.0,
-            Value::Boolean(b) => *b,
-            Value::Nil => false,
+            StackValue::Integer(i) => *i != 0,
+            StackValue::Float(f) => *f != 0.0,
+            StackValue::Boolean(b) => *b,
+            StackValue::Nil => false,
+            StackValue::Object(_) => false,
         }
     }
-
 }
 
 pub struct BytecodeChunk {
     pub code: Vec<u8>,
-    pub constants: Vec<Value>,
+    pub constants: Vec<ConstantsValue>,
 }
 impl BytecodeChunk {
-    pub fn new(code: Vec<u8>, constants: Vec<Value>) -> Self {
+    pub fn new(code: Vec<u8>, constants: Vec<ConstantsValue>) -> Self {
         BytecodeChunk { code, constants }
     }
 }
-
 
 // struct ByteCodeFunction {
 //     _name: String,
@@ -53,15 +75,16 @@ impl BytecodeChunk {
 #[repr(u8)]
 #[derive(Debug, PartialEq, Clone, IntoPrimitive, TryFromPrimitive)]
 pub enum Op {
-    Load = 0x00,
-    Add = 0x01,
-    Sub = 0x02,
-    Mul = 0x03,
-    Div = 0x04,
-    Neg = 0x05,
-    Jump = 0x06,     // jumps to the specified address
-    CondJump = 0x07, // jumps to the specified address if the top of the stack is not zero
-    FuncCall = 0x08,
+    Load = 0,
+    Add = 1,
+    Sub = 2,
+    Mul = 3,
+    Div = 4,
+    Neg = 5,
+    Jump = 6,     // jumps to the specified address
+    CondJump = 7, // jumps to the specified address if the top of the stack is not zero
+    FuncCall = 8,
+    DebugPrint = 255, // prints the stack
 }
 
 impl VM {
@@ -100,7 +123,6 @@ impl VM {
                     self.advance();
                 }
                 Op::CondJump => {
-                    // get the jump offset
                     let mut offset = self.consume_next_byte_as_byte() as usize;
                     let cond_val = self.stack.pop().unwrap();
 
@@ -108,24 +130,19 @@ impl VM {
                         offset = 1;
                     };
 
-                    // println!("offset: {offset}");
-                    // println!("befor ip: {:?}", ip);
                     self.ip = unsafe { self.ip.add(offset) };
-                    // println!("after ip: {:?}", ip);
-
                 }
                 Op::Jump => {
                     let offset = self.consume_next_byte_as_byte() as usize;
-                    // println!("offset: {offset}");
-                    // println!("befor ip: {:?}", ip);
                     self.ip = unsafe { self.ip.add(offset) };
-                    // println!("after ip: {:?}", ip);
                 }
                 Op::Add => {
                     let a = self.stack.pop().unwrap();
                     let b = self.stack.pop().unwrap();
                     self.stack.push(match (a, b) {
-                        (Value::Integer(a), Value::Integer(b)) => Value::Integer(a + b),
+                        (StackValue::Integer(a), StackValue::Integer(b)) => {
+                            StackValue::Integer(a + b)
+                        }
                         _ => todo!(),
                     });
                     self.advance();
@@ -134,7 +151,9 @@ impl VM {
                     let a = self.stack.pop().unwrap();
                     let b = self.stack.pop().unwrap();
                     self.stack.push(match (a, b) {
-                        (Value::Integer(a), Value::Integer(b)) => Value::Integer(a - b),
+                        (StackValue::Integer(a), StackValue::Integer(b)) => {
+                            StackValue::Integer(a - b)
+                        }
                         _ => todo!(),
                     });
                     self.advance();
@@ -143,7 +162,9 @@ impl VM {
                     let a = self.stack.pop().unwrap();
                     let b = self.stack.pop().unwrap();
                     self.stack.push(match (a, b) {
-                        (Value::Integer(a), Value::Integer(b)) => Value::Integer(a * b),
+                        (StackValue::Integer(a), StackValue::Integer(b)) => {
+                            StackValue::Integer(a * b)
+                        }
                         _ => todo!(),
                     });
                     self.advance();
@@ -152,7 +173,9 @@ impl VM {
                     let a = self.stack.pop().unwrap();
                     let b = self.stack.pop().unwrap();
                     self.stack.push(match (a, b) {
-                        (Value::Integer(a), Value::Integer(b)) => Value::Integer(a / b),
+                        (StackValue::Integer(a), StackValue::Integer(b)) => {
+                            StackValue::Integer(a / b)
+                        }
                         _ => todo!(),
                     });
                     self.advance();
@@ -160,12 +183,12 @@ impl VM {
                 Op::Neg => {
                     let a = self.stack.pop().unwrap();
                     self.stack.push(match a {
-                        Value::Integer(a) => Value::Integer(-a),
+                        StackValue::Integer(a) => StackValue::Integer(-a),
                         _ => todo!(),
                     });
                     self.advance();
                 }
-                
+
                 // Op::FuncCall => todo!(),
                 // Op::FuncCall => {
                 //     // expects the stack to be (top-down):
@@ -177,15 +200,32 @@ impl VM {
                 //         arity,
                 //     } = &self.function_table[func_idx as usize];
                 // }
+                Op::DebugPrint => {
+                    println!("{:?}", self.stack);
+                    self.advance();
+                }
                 _ => todo!(),
             }
         }
     }
 
-    fn consume_next_byte_as_constant(&mut self, chunk: &BytecodeChunk) -> Value {
+    fn consume_next_byte_as_constant(&mut self, chunk: &BytecodeChunk) -> StackValue {
         unsafe {
             self.ip = self.ip.add(1);
-            chunk.constants[*self.ip as usize]
+            match chunk.constants[*self.ip as usize].clone() {
+                ConstantsValue::Integer(v) => StackValue::Integer(v),
+                ConstantsValue::Float(v) => StackValue::Float(v),
+                ConstantsValue::Boolean(v) => StackValue::Boolean(v),
+                ConstantsValue::Nil => StackValue::Nil,
+                ConstantsValue::Object(value) => {
+                    // let obj_ptr = allocate(HeapObject {
+                    //     next: std::ptr::null_mut(),
+                    //     value,
+                    // });
+                    let obj_ptr = allocate_value(value);
+                    StackValue::Object(obj_ptr)
+                }
+            }
         }
     }
 
@@ -203,6 +243,22 @@ impl VM {
     }
 }
 
+unsafe fn allocate<T>(obj: T) -> *mut T {
+    let obj_ptr = alloc(Layout::new::<T>()) as *mut T;
+    obj_ptr.write(obj);
+    obj_ptr
+}
+
+unsafe fn allocate_value(obj_value: ObjectValue) -> *mut HeapObject {
+    let obj_ptr = alloc(Layout::new::<HeapObject>()) as *mut HeapObject;
+    obj_ptr.write(HeapObject {
+        next: std::ptr::null_mut(),
+        value: obj_value,
+    });
+    println!("LEAKING MEMORY allocated {:?}", obj_ptr);
+    obj_ptr
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -212,10 +268,10 @@ mod tests {
         let mut vm = VM::new();
         let chunk = BytecodeChunk {
             code: vec![0x00, 0x00],
-            constants: vec![Value::Integer(5)],
+            constants: vec![ConstantsValue::Integer(5)],
         };
         vm.run(chunk);
-        assert_eq!(vm.stack, vec![Value::Integer(5)])
+        assert_eq!(vm.stack, vec![StackValue::Integer(5)])
     }
 
     #[test]
@@ -224,34 +280,39 @@ mod tests {
         // push 5 push 6 add
         // 5 + 6 = 11
         let chunk = BytecodeChunk {
-            code: vec![
-                Op::Load.into(), 0,
-                Op::Load.into(), 1,
-                Op::Add.into(),
-            ],
-            constants: vec![Value::Integer(5), Value::Integer(6)],
+            code: vec![Op::Load.into(), 0, Op::Load.into(), 1, Op::Add.into()],
+            constants: vec![ConstantsValue::Integer(5), ConstantsValue::Integer(6)],
         };
         vm.run(chunk);
-        assert_eq!(vm.stack[0], Value::Integer(11))
+        assert_eq!(vm.stack[0], StackValue::Integer(11))
     }
 
     #[test]
     fn test_cond() {
         let bytecode = vec![
-            Op::Load.into(), 0,
-            Op::CondJump.into(), 5, // jump to the load
-            Op::Load.into(), 1,
-            Op::Jump.into(), 3, // jump to the end
-            Op::Load.into(), 2,
+            Op::Load.into(),
+            0,
+            Op::CondJump.into(),
+            5, // jump to the load
+            Op::Load.into(),
+            1,
+            Op::Jump.into(),
+            3, // jump to the end
+            Op::Load.into(),
+            2,
         ];
         let ptr = bytecode.as_ptr();
 
         let mut vm = VM::new();
         vm.run(BytecodeChunk {
             code: bytecode,
-            constants: vec![Value::Integer(1), Value::Integer(3), Value::Integer(2)],
+            constants: vec![
+                ConstantsValue::Integer(1),
+                ConstantsValue::Integer(3),
+                ConstantsValue::Integer(2),
+            ],
         });
-        assert_eq!(vm.stack, vec![Value::Integer(2)]);
+        assert_eq!(vm.stack, vec![StackValue::Integer(2)]);
         assert_eq!(vm.ip, unsafe { ptr.add(10) }); // idx after the last byte
     }
 
@@ -259,27 +320,60 @@ mod tests {
     fn test_cond_not() {
         let chunk = BytecodeChunk {
             code: vec![
-                Op::Load.into(), 0,
-                Op::CondJump.into(), 5,
-                Op::Load.into(), 1,
-                Op::Jump.into(), 3,
-                Op::Load.into(), 2,
+                Op::Load.into(),
+                0,
+                Op::CondJump.into(),
+                5,
+                Op::Load.into(),
+                1,
+                Op::Jump.into(),
+                3,
+                Op::Load.into(),
+                2,
             ],
-            constants: vec![Value::Integer(0), Value::Integer(3), Value::Integer(2)],
+            constants: vec![
+                ConstantsValue::Integer(0),
+                ConstantsValue::Integer(3),
+                ConstantsValue::Integer(2),
+            ],
         };
         let ptr = chunk.code.as_ptr();
 
         let mut vm = VM::new();
         vm.run(chunk);
-        assert_eq!(vm.stack, vec![Value::Integer(3)]);
+        assert_eq!(vm.stack, vec![StackValue::Integer(3)]);
         assert_eq!(vm.ip, unsafe { ptr.add(10) });
+    }
+
+    #[test]
+    fn test_string() {
+        let chunk = BytecodeChunk {
+            code: vec![Op::Load.into(), 0],
+            constants: vec![ConstantsValue::Object(ObjectValue::String(
+                "Hello, world!".to_string(),
+            ))],
+        };
+        let ptr = chunk.code.as_ptr();
+
+        let mut vm = VM::new();
+        vm.run(chunk);
+        assert_eq!(vm.stack.len(), 1);
+
+        let string = match vm.stack[0] {
+            StackValue::Object(ptr) => match &unsafe { &*ptr }.value {
+                ObjectValue::String(str) => str,
+            }
+            _ => panic!(),
+        };
+
+        assert_eq!(string, "Hello, world!");
+        assert_eq!(vm.ip, unsafe { ptr.add(2) });
     }
 }
 
-
 // not needed for now
 // // #[repr(C)] // for the struct definition
-// impl Value {
+// impl StackValue {
 //     fn from_bytes(bytes: [u8; 16]) -> Self {
 //         unsafe { mem::transmute(bytes) }
 //     }
